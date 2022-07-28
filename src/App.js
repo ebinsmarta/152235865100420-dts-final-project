b32057a8be6dd2835ac799541be3171ea85d4ea5
ebinsimarmata <ebin.smarta@gmail.com>
2022-07-28 16:24:51 +0700

    testing

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          HERBINTON BAKTIAR SIMARMATA - 152235865100420
         </p>
         <a
           className="App-link"
